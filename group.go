6fd8d3c0209ca042cfa562687c3908c2c10f32d7
Mohammad Rajabloo <themrsoftware@gmail.com>
2024-06-27 21:07:11 +0330

    add error group support

---
+++ group.go
@@ -0,0 +1,54 @@
+package errors
+
+import (
+	"sync"
+)
+
+// Group is sync.WaitGroup with error support.
+type Group struct {
+	noCopy noCopy
+	wg     sync.WaitGroup
+	errors MultiError
+}
+
+// NewGroup create new Group.
+func NewGroup() Group {
+	return Group{wg: sync.WaitGroup{}}
+}
+
+// Wait is sync.WaitGroup.Wait.
+func (g *Group) Wait() {
+	g.wg.Wait()
+}
+
+// Add is sync.WaitGroup.Add.
+func (g *Group) Add(delta int) {
+	g.wg.Add(delta)
+}
+
+// Done is sync.WaitGroup.Done, but is support error as parameter.
+func (g *Group) Done(err error) {
+	g.wg.Done()
+
+	if err == nil {
+		return
+	}
+
+	g.errors.Add(err)
+}
+
+// Err return all happened error as a single error.
+func (g *Group) Err() error {
+	if g.errors.Len() == 0 {
+		return nil
+	}
+
+	return &g.errors
+}
+
+// noCopy may be embedded into structs which must not be copied
+// after the first use.
+//
+// See https://golang.org/issues/8005#issuecomment-190753527
+// for details.
+type noCopy struct{}
